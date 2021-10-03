16ad98d4df85227c0f1f6e21e4bd8672792f10f5
= <20021460@vnu.edu.vn>
2021-10-03 21:28:32 +0700

    command line 2( thieu dictionary search)

--- Dictionary.java
+++ Dictionary.java
@@ -1,15 +1,16 @@
 package Dictionary;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 class Dictionary {
-    ArrayList<Word> wordArray = new ArrayList<>();
-
+    //    ArrayList<Word> wordArray = new ArrayList<>();
+    Map<String, String> wordArray = new HashMap<>();
     Scanner sc = new Scanner(System.in);
-    
+
     public void insertFromCommandline() {
         System.out.println("Nhap vao so luong tu muon nhap: ");
 
@@ -23,26 +24,36 @@ class Dictionary {
             System.out.println("Nhap vao tu giai nghia: ");
             String vn = sc.nextLine();
 
-            Word w = new Word(eng, vn);
-            this.wordArray.add(w);
+//            Word w = new Word(eng, vn);
+//            this.wordArray.add(w);
+            wordArray.put(eng, vn);
         }
 
-        Collections.sort(this.wordArray);
+//        Collections.sort(this.wordArray); 
+
+
     }
 
     public void showAllWords() {
+//        System.out.println(this.wordArray.toString());
         System.out.println("Danh sach tat ca cac tu dang co trong tu dien: ");
         System.out.println("No  | English           | Vietnamese");
 
-        for (int i = 0; i < this.wordArray.size(); i++) {
-            System.out.println((i + 1) + "  | " + this.wordArray.get(i).getTarget() + "           | "
-                    + this.wordArray.get(i).getExplain());
-        }
+//        for (int i = 0; i < this.wordArray.size(); i++) { 
+//            System.out.println((i + 1) + "  | " + this.wordArray.get(i).getTarget() + "           | " 
+//                    + this.wordArray.get(i).getExplain()); 
+//        }
+        AtomicInteger index = new AtomicInteger(1);
+        this.wordArray.forEach((key, value) -> {
+            System.out.println(index + "  | " + key + "           | " + value);
+            index.addAndGet(1);
+        });
     }
 
     public void insertFromFile() {
         try {
-            File wordFile = new File("src\\Dictionary\\dictionaries.txt");
+            File wordFile;
+            wordFile = new File("dictionary\\src\\dictionaries.txt");
             Scanner fileReader = new Scanner(wordFile);
 
             while (fileReader.hasNextLine()) {
@@ -50,40 +61,95 @@ class Dictionary {
                 content.trim();
                 String[] postSplit = content.split("    "); // Lấy từ tiếng Anh và tiếng Việt
 
-                Word w = new Word(postSplit[0], postSplit[1]);
-                this.wordArray.add(w);
+//                Word w = new Word(postSplit[0], postSplit[1]);
+                wordArray.put(postSplit[0], postSplit[1]);
             }
 
-            Collections.sort(this.wordArray);
+//            Collections.sort(this.wordArray);
         } catch (FileNotFoundException e) {
             System.out.println("Error. Khong tim thay file.");
         }
     }
 
-    private String wordBinarySearch(String target) {
-        int left = 0;
-        int right = this.wordArray.size() - 1;
-
-        while (left <= right) {
-            int mid = right + (left - right) / 2;
-
-            if (target.compareTo(this.wordArray.get(mid).getTarget()) == 0) {
-                return wordArray.get(mid).getExplain();
-            } else if (target.compareTo(this.wordArray.get(mid).getTarget()) > 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-
-        return "Khong tim thay ket qua mong muon";
-    }
+//    private String wordBinarySearch(String target) {
+////        int left = 0;
+////        int right = this.wordArray.size() - 1;
+////
+//////        while (left <= right) {
+//////            int mid = right + (left - right) / 2;
+//////
+//////            if (target.compareTo(this.wordArray.get(mid).getTarget()) == 0) {
+//////                return wordArray.get(mid).getExplain();
+//////            } else if (target.compareTo(this.wordArray.get(mid).getTarget()) > 0) {
+//////                left = mid + 1;
+//////            } else {
+//////                right = mid - 1;
+//////            }
+//////        }
+//        if (this.wordArray.containsKey(target)) {
+//            return this.wordArray.get(target);
+//        } else
+//
+//            return "Khong tim thay ket qua mong muon";
+//    }
 
     public void dictionaryLookup() {
         System.out.println("Nhap tu ma ban muon tra nghia: ");
         String target = sc.nextLine();
-
-        System.out.println(this.wordBinarySearch(target));
-
+        String value = this.wordArray.get(target);
+//        System.out.println(this.wordBinarySearch(target));
+        System.out.println(value);
     }
+
+    public boolean dictionaryEdit() {
+        boolean check = false;
+        System.out.println("1. Xoa tu");
+        System.out.println("2. Sua tu");
+        System.out.println("Chon thao tac ma ban muon sua: ");
+        int key = sc.nextInt();
+        if (key == 1) {
+            System.out.print("Nhap tu muon xoa: ");
+            String delWord = sc.next();
+//            for (int i = 0; i < this.wordArray.size(); i++) {
+//                if (delWord.equals(this.wordArray.get(i).getTarget())) {
+//                    this.wordArray.remove(i);
+//                    break;
+//                } else {
+//                    System.out.println("Khong tim thay ket qua mong muon");
+//                }
+//            }
+            if (wordArray.containsKey(delWord)) {
+                this.wordArray.remove(delWord);
+                check = true;
+            } else
+                check = false;
+        } else if (key == 2) {
+            System.out.print("Nhap tu muon sua: ");
+            String editWord = sc.next();
+//            for (int i = 0; i < this.wordArray.size(); i++) {
+//                if (delWord.equals(this.wordArray.get(i).getTarget())) {
+//                    System.out.print("Sua lai nghia cua tu: ");
+//                    String exWord = sc.nextLine();
+//                    this.wordArray.get(i).setExplain(exWord);
+//                    break;
+//                } else {
+//                    System.out.println("Khong tim thay ket qua mong muon");
+//                }
+//            }
+            if (this.wordArray.containsKey(editWord)) {
+                Scanner cin = new Scanner(System.in);
+                System.out.print("Sua lai nghia cua tu: ");
+                String newMeaningWord = cin.nextLine();
+                this.wordArray.put(editWord, newMeaningWord);
+                check = true;
+            } else
+                check = false;
+        }
+        return check;
+    }
+
+        public void exportFile() {
+
+        }
+
 }
--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,13 +1,23 @@
 package Dictionary;
+
 class DictionaryCommandLine {
-    public static void dictionaryBasic(Dictionary dict) {
-        dict.insertFromCommandline();
-        dict.showAllWords();
-    }
+//    public static void dictionaryBasic(Dictionary dict) {
+//        dict.insertFromCommandline();
+//        dict.showAllWords();
+//    }
 
     public static void dictionaryAdvanced(Dictionary dict) {
         dict.insertFromFile();
-        dict.showAllWords(); 
-         dict.dictionaryLookup();
+        dict.insertFromCommandline();
+        dict.showAllWords();
+        dict.dictionaryLookup();
+        boolean x = dict.dictionaryEdit();
+        if (x) {
+            dict.showAllWords();
+        }
+//        dict.exportFile();
+
+
     }
+
 }
